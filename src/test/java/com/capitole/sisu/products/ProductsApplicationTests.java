98102f49dfdaa49576f3f637937af80b213806b4
edelgado <edeldelgado90@gmail.com>
2025-03-19 11:35:16 +0100

    feat: Initialize Spring Boot application with pom and basic structure

---
+++ src/test/java/com/capitole/sisu/products/ProductsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.capitole.sisu.products;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+class ProductsApplicationTests {
+
+    @Test
+    void contextLoads(ApplicationContext context) {
+        assertThat(context).isNotNull();
+    }
+}
